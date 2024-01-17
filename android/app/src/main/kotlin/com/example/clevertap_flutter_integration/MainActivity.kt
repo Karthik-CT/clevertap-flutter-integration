facad1f1de920c899e792a4e747eb4a6f1211338
Karthik Iyer <100284000+Karthik-CT@users.noreply.github.com>
2024-01-17 17:47:54 +0530

    added dismiss notification support

--- android/app/src/main/kotlin/com/example/clevertap_flutter_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/clevertap_flutter_integration/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.clevertap_flutter_integration
 
+import android.app.NotificationManager
 import io.flutter.embedding.android.FlutterActivity
 import android.content.Context
 import android.os.Bundle
@@ -19,6 +20,7 @@ import android.util.Log
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
+import com.clevertap.android.pushtemplates.PTConstants
 import com.clevertap.android.sdk.CTInboxListener
 import com.clevertap.android.sdk.CTInboxStyleConfig
 import io.flutter.plugin.common.MethodChannel
@@ -37,6 +39,40 @@ class MainActivity: FlutterActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             // CleverTapAPI.getDefaultInstance(this)?.pushNotificationClickedEvent(intent!!.extras)
             cleverTapDefaultInstance?.pushNotificationClickedEvent(intent!!.extras)
+        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            NotificationUtils.dismissNotification(intent, applicationContext)
+//        }
+
+    }
+}
+
+object NotificationUtils {
+
+    //Require to close notification on action button click
+    fun dismissNotification(intent: Intent?, applicationContext: Context){
+        intent?.extras?.apply {
+            var autoCancel = true
+            var notificationId = -1
+
+            getString("actionId")?.let {
+                Log.d("ACTION_ID", it)
+                autoCancel = getBoolean("autoCancel", true)
+                notificationId = getInt("notificationId", -1)
+            }
+            /**
+             * If using InputBox template, add ptDismissOnClick flag to not dismiss notification
+             * if pt_dismiss_on_click is false in InputBox template payload. Alternatively if normal
+             * notification is raised then we dismiss notification.
+             */
+            val ptDismissOnClick = intent.extras!!.getString(PTConstants.PT_DISMISS_ON_CLICK,"")
+
+            if (autoCancel && notificationId > -1 && ptDismissOnClick.isNullOrEmpty()) {
+                val notifyMgr: NotificationManager =
+                    applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notifyMgr.cancel(notificationId)
+            }
         }
     }
 }
