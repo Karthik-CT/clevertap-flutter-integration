c1628188b4a6d53a106a17a543875bf205f79c82
KarthikCT <karthik.iyer@clevertap.com>
2024-05-06 18:51:57 +0530

    added Push Templates

---
+++ ios/NotificationContentDemo/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  NotificationContentDemo
+//
+//  Created by Karthik Iyer on 06/05/24.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
