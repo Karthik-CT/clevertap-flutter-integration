5f66052aaf8270529b2c78ad54ac14ba00a76be0
Karthik-CT <karthik.iyer@clevertap.com>
2026-03-17 12:41:34 +0530

    latest commit

---
+++ android/CT-Templates/build/generated/ap_generated_sources/release/out/com/clevertap/ct_templates/BR.java
@@ -0,0 +1,5 @@
+package com.clevertap.ct_templates;
+
+public class BR {
+  public static int _all = 0;
+}
---
+++ android/CT-Templates/build/generated/data_binding_trigger/debug/com/clevertap/ct_templates/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.clevertap.ct_templates;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
